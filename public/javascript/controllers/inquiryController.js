a4432d088c765ae38f61c9f581d726103c5afdf5
Jeffrey D <literallyelvis@gmail.com>
2015-11-29 10:07:39 -0600

    Some voting regulations

--- public/javascript/controllers/inquiryController.js
+++ public/javascript/controllers/inquiryController.js
@@ -9,12 +9,29 @@ app.controller('inquiryController', [
         $scope.answers = data.data;
       })
 
-      $scope.upvoteAnswer = function(id){
-        console.log('upvoteAnswer called for answer: ' + id);
+      $scope.userVote = 0;
+      $scope.upvoteAnswer = function(answer){
+        console.log('upvoteAnswer called for answer: ' + answer.id);
+        if( $scope.userVote === -1 ){
+          $scope.userVote = 1;
+          answer.upvotes += 1;
+          answer.downvotes -= 1;
+        } else if( $scope.userVote === 0 ){
+          $scope.userVote = 1;
+          answer.upvotes += 1;
+        }
       }
 
-      $scope.downvoteAnswer = function(id){
-        console.log('downvoteAnswer called for answer: ' + id);
+      $scope.downvoteAnswer = function(answer){
+        console.log('downvoteAnswer called for answer: ' + answer.id);
+        if( $scope.userVote === 1 ){
+          $scope.userVote = -1;
+          answer.downvotes += 1;
+          answer.upvotes -= 1;
+        } else if( $scope.userVote === 0 ){
+          $scope.userVote = -1;
+          answer.downvotes -= 1;
+        }
       }
     }
 ]);
