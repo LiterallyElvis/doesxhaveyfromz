b3f682ec6c8bca1441f1db3a9fd2683e64d17785
Jeffrey D <literallyelvis@gmail.com>
2015-12-06 15:23:46 -0600

    Some UI fixes

--- public/javascript/controllers/inquiryController.js
+++ public/javascript/controllers/inquiryController.js
@@ -9,6 +9,10 @@ app.controller('inquiryController', [
 
       $http.get('/api/inquiry/' + $scope.answerId + '/answers').then(function(data){
         $scope.answers = data.data;
+
+        console.log('answers:')
+        console.log(JSON.stringify($scope.answers, null, 4))
+
         $scope.x = $scope.answers[0].x
         $scope.y = $scope.answers[0].y
         $scope.z = $scope.answers[0].z
@@ -16,7 +20,7 @@ app.controller('inquiryController', [
 
       $scope.toggleText = "show examples ▶";
       // this could be a user-setting one day if I cared to do such a thing.
-      $scope.showExamples = true;
+      $scope.showExamples = false;
       $scope.toggleExamples = function(){
         // ternary operators make me feel smart
         $scope.showExamples = $scope.showExamples ? false : true;
@@ -24,28 +28,32 @@ app.controller('inquiryController', [
       }
 
       // these functions look like a binary shart.
-      $scope.userVote = 0;
+      $scope.userVotes = {};
       $scope.upvoteAnswer = function(answer){
-        console.log('upvoteAnswer called for answer: ' + answer.id);
-        if( $scope.userVote === -1 ){
-          $scope.userVote = 1;
-          answer.upvotes += 1;
-          answer.downvotes -= 1;
-        } else if( $scope.userVote === 0 ){
-          $scope.userVote = 1;
+        if( $scope.userVotes[answer.id] ){
+          // user has already voted for this answer.
+          if( $scope.userVotes[answer.id] != 1 ){
+            $scope.userVotes[answer.id] = 1;
+            answer.upvotes += 1;
+            answer.downvotes -= 1;
+          }
+        } else {
+          $scope.userVotes[answer.id] = 1;
           answer.upvotes += 1;
         }
       }
 
       $scope.downvoteAnswer = function(answer){
-        console.log('downvoteAnswer called for answer: ' + answer.id);
-        if( $scope.userVote === 1 ){
-          $scope.userVote = -1;
+        if( $scope.userVotes[answer.id] ){
+          // user has already voted for this answer.
+          if( $scope.userVotes[answer.id] != -1 ){
+            $scope.userVotes[answer.id] = -1;
+            answer.upvotes -= 1;
+            answer.downvotes += 1;
+          }
+        } else {
+          $scope.userVotes[answer.id] = -1;
           answer.downvotes += 1;
-          answer.upvotes -= 1;
-        } else if( $scope.userVote === 0 ){
-          $scope.userVote = -1;
-          answer.downvotes -= 1;
         }
       }
     }
