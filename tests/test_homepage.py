d576b639416dc30567fb17d83311ec1d8be88fdb
Jeffrey D <literallyelvis@gmail.com>
2016-09-29 22:04:27 -0500

    I think these are all the tests for the homepage.

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -6,7 +6,8 @@ from unittest import TestCase
 
 class TestHomePage(TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome('./chromedriver')
+        chromedriver = './chromedriver'
+        self.driver = webdriver.Chrome(chromedriver)
 
     def shutDown(self):
         self.driver.quit()
@@ -23,6 +24,23 @@ class TestHomePage(TestCase):
         self.assertIsNotNone(self.driver.find_element_by_id('anyWarning'))
         self.driver.close()
 
+    def test_typing_duplicate_entries_shows_warning(self):
+        self.driver.get('http://localhost:3000')
+
+        with self.assertRaises(Exception) as cm:
+            self.assertIsNone(self.driver.find_element_by_id('anyWarning'))
+
+        x = self.driver.find_element_by_id('x')
+        z = self.driver.find_element_by_id('z')
+        s = self.driver.find_element_by_id('submitButton')
+        
+        x.send_keys('python')
+        z.send_keys('python')
+        s.click()
+
+        self.assertIsNotNone(self.driver.find_element_by_id('invalidWarning'))
+        self.driver.close()
+
     def test_typing_nothing_shows_warning(self):
         self.driver.get('http://localhost:3000')
 
@@ -33,4 +51,28 @@ class TestHomePage(TestCase):
         x.click()
 
         self.assertIsNotNone(self.driver.find_element_by_id('invalidWarning'))
+        self.driver.close()
+    
+    def test_proper_entries_work(self):
+        self.driver.get('http://localhost:3000')
+
+        with self.assertRaises(Exception) as cm:
+            self.assertIsNone(self.driver.find_element_by_id('anyWarning'))
+
+        x = self.driver.find_element_by_id('x')
+        y = self.driver.find_element_by_id('y')
+        z = self.driver.find_element_by_id('z')
+        s = self.driver.find_element_by_id('submitButton')
+        
+        x.send_keys('Ruby')
+        y.send_keys('.get()')
+        z.send_keys('Python')
+
+        with self.assertRaises(Exception) as cm:
+            self.assertIsNone(self.driver.find_element_by_id('anyWarning'))
+            self.assertIsNone(self.driver.find_element_by_id('invalidWarning'))
+
+        s.click()
+
+        self.assertEqual(self.driver.current_url, 'http://localhost:3000/search?x=Ruby&y=.get()&z=Python')
         self.driver.close()
